60415c63a18781953b9928d04295f4730c31ff47
Oliver Wilkie <oliverwilkie@gmail.com>
2013-09-10 22:13:22 +0100

    Added email info

--- app.rb
+++ app.rb
@@ -1,13 +1,22 @@
 require 'sinatra'
 require 'pony'
 
+Pony.options = { :from => 'info@peekaboo.com', 
+				:via => :smtp, 
+				:via_options => { :host => ENV['POSTMARK_SMTP_SERVER'], 
+									:port => '25', 
+									:enable_starttls_auto => true,
+									:user_name => ENV['POSTMARK_API_KEY'],
+									:domain => 'salty-headland-5059.heroku.com',
+    								:password => ENV['POSTMARK_API_KEY'],
+    								:authentication => :plain} }
+
 get '/' do
 	erb :index
 end
 
 post '/thanks' do
   	Pony.mail :to => params[:email],
-            :from => "info@peekaboo.com",
             :subject => "Test, #{params[:name]}!",
             :body => erb(:email)
 	erb :thanks

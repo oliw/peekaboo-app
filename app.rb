0e090ebacff52d49cb6f1935baa9f8da061999fc
Oliver Wilkie <oliverwilkie@gmail.com>
2013-09-10 22:02:06 +0100

    Prepared for heroku

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require 'pony'
 
 get '/' do
 	erb :index
 end
 
 post '/thanks' do
+  	Pony.mail :to => params[:email],
+            :from => "info@peekaboo.com",
+            :subject => "Test, #{params[:name]}!",
+            :body => erb(:email)
 	erb :thanks
 end
